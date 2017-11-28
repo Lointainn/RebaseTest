5f4a2a31ead2625b3df5631bd7305a1a8249f086
Wu <wuzs@linkstec.com>
2017-11-28 11:14:57 +0800

    no message

--- RebaseTest/ViewController.m
+++ RebaseTest/ViewController.m
@@ -17,8 +17,10 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
-    //1
+    //1.
     //2
+    //3.3
+    //2.2
     //3
     //4
     //5
